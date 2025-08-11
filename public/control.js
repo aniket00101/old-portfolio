352a28dc7707ec2d107b2692de05de4c817a0bb2
aniket00101 <dasaniket971@gmail.com>
2025-08-11 11:09:48 +0530

    added env

--- public/control.js
+++ public/control.js
@@ -5,13 +5,13 @@
     } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
     const firebaseConfig = {
-        apiKey: "AIzaSyBWWiaI-rMq0wYNUsiG87hYn_MRXF8NX7U",
-        authDomain: "portfolio-26420.firebaseapp.com",
-        projectId: "portfolio-26420",
-        storageBucket: "portfolio-26420.firebasestorage.app",
-        messagingSenderId: "437850469315",
-        appId: "1:437850469315:web:6cac2226c35132c61e4376",
-        measurementId: "G-3LMD0F153Z"
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+        measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
     };
 
     const app = initializeApp(firebaseConfig);
